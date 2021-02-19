60e3dfb1726e86d4ebaead5baca09b01a06685ec
Elias Calagiu <eliasc81@gmail.com>
2021-02-19 04:54:27 -0800

    app is now generating based only on first preference chosen true but only returning one character

--- script.js
+++ script.js
@@ -16,35 +16,28 @@ var passCharNum = ["0123456789"];
 var passCharSpec = ["!@#$%^&*()"];
 
 // TODO: create an empty array to hold user requested characters
-var finalArray = [];
 
 // TODO: console.log every step of the way
 
 // TODO: Create generatePassword function and write all the logic within this function
 
 function generatePassword() {
-
+  var finalArray = "";
+  var finalPassword = ""
   // TODO: Create confirm to ask user if they want capital letters and set to a variable
   var capitalLetters = confirm("Would you like capital letters in your password?");
-  if (capitalLetters === true) {
-    finalArray.push("Capital")
-  }
+  
+  
   // TODO: Create confirm to ask user if they want lowercase letters and set to a variable
   var lowerLetters = confirm("Would you like lowercase letters in your password?");
-  if (lowerLetters === true) {
-    finalArray.push("Lower")
-  }
+  
   // TODO: Create confirm to ask user if they want numbers and set to a variable
   var num = confirm("Would you like numbers in your password?");
-  if (num === true) {
-    finalArray.push("Number")
-  }
+  
 
   // // TODO: Create confirm to ask user if they want special characters and set to a variable
   var specialCharQ = confirm("Would you like special chars in your password?");
-  if (specialCharQ === true) {
-    finalArray.push("Special")
-  }
+  
 
   // // TODO: create a conditional to make sure the user has chosen at least one type of character
   // if (capitalLetters, lowerLetters, num, specialCharQ===true) {
@@ -67,15 +60,15 @@ alert("You have to choose one ya dingus");
   var passLength = prompt("How long would you like your password to be? (between 8-128 characters)");
   if (passLength < 8) {
     alert("must be 8 characters or above");
-    return;
+    // return;
   }
   if (passLength > 128) {
     alert("must be 128 characters or less");
-    return;
+    // return;
   }
   if (isNaN(passLength) === true) {
     alert("gotta pick a number nerd");
-    return;
+    // return;
   }
   console.log(passLength);
 
@@ -87,37 +80,59 @@ alert("You have to choose one ya dingus");
 
 
   // TODO: Create multiple if statements that checks if user said yes or no to different confirms that we asked them, and based on their response push those specific characters to our empty array
-  if (finalArray.length > 0) {
-    var finalPassword = ""
-    for (var i = 0; i < passLength; i++) {
-      var passCharSelected = "";
-      var passCharType = finalArray[Math.floor(Math.random() * finalArray.length)]
 
-      if (passCharType === "Capital") {
-        passCharSelected = passCharUpper[Math.floor(Math.random() * passCharUpper.length)]
-
-        console.log(passCharSelected);
-      }
-
-      else if (passCharType === "Lower") {
-        passCharSelected = passCharLower[Math.floor(Math.random() * passCharLower.length)]
-
-        console.log(passCharSelected);
-      }
-      else if(passCharType === "Number") {
-       passCharSelected=passCharNum[Math.floor(Math.random()*passCharNum.length)]
-
-      console.log(passCharSelected);
-      }
-      else if(passCharType === "Special") {
-       passCharSelected=passCharSpec[Math.floor(Math.random()*passCharSpec.length)]
-
-      console.log(passCharSelected);
-      finalPassword = finalPassword + passCharSelected
-      console.log(finalPassword);
-    }
-    return finalPassword;
+  if (capitalLetters === true) {
+    finalArray= finalArray.concat(passCharUpper)
   }
+    if (lowerLetters === true) {
+      finalArray=finalArray.concat(passCharLower)
+    }
+
+    if (num === true) {
+      finalArray= finalArray.concat(passCharNum)
+    }
+
+    if (specialCharQ === true) {
+      finalArray= finalArray.concat(passCharSpec)
+    }
+
+
+    var randomCapital = Math.floor(Math.random() * finalArray.length)
+    var randomLower = Math.floor(Math.random() * finalArray.length)
+    var randomNum = Math.floor(Math.random() * finalArray.length)
+    var randomSpecial = Math.floor(Math.random() * finalArray.length)
+
+    for (var i = 0; i < passLength; i++) {
+//       var passCharSelected = "";
+      var passCharType = finalArray[Math.floor(Math.random() * finalArray.length)]
+      finalPassword = finalPassword.concat(passCharType)
+      console.log(passCharType);
+//       if (passCharType === "Capital") {
+//         passCharSelected = passCharUpper[Math.floor(Math.random() * passCharUpper.length)]
+
+//         console.log(passCharSelected);
+//       }
+
+//       else if (passCharType === "Lower") {
+//         passCharSelected = passCharLower[Math.floor(Math.random() * passCharLower.length)]
+
+//         console.log(passCharSelected);
+//       }
+//       else if(passCharType === "Number") {
+//        passCharSelected=passCharNum[Math.floor(Math.random()*passCharNum.length)]
+
+//       console.log(passCharSelected);
+//       }
+//       else if(passCharType === "Special") {
+//        passCharSelected=passCharSpec[Math.floor(Math.random()*passCharSpec.length)]
+
+//       console.log(passCharSelected);
+//       finalPassword = finalPassword + passCharSelected
+//       console.log(finalPassword);
+//     }
+      console.log(finalPassword);
+    return finalPassword;
+//   }
 }
 
 }
